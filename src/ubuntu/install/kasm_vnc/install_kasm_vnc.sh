9eb36231706920edd44d13b25f3f62937646a4f5
Matthew McClaskey <matt@kasmweb.com>
2025-08-15 17:09:25 +0000

    VNC-238 udpate kasmvnc

--- src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
+++ src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
@@ -17,7 +17,7 @@ echo "Install KasmVNC server"
 cd /tmp
 BUILD_ARCH=$(uname -m)
 UBUNTU_CODENAME=""
-COMMIT_ID="e742e463635851b5f8ce073a686900819901cbd7"
+COMMIT_ID="9121826614073f22cfe78951677ec226bcf720e4"
 BRANCH="release" # just use 'release' for a release branch
 KASMVNC_VER="1.4.0"
 COMMIT_ID_SHORT=$(echo "${COMMIT_ID}" | cut -c1-6)
