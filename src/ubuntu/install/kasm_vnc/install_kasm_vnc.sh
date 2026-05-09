ae6d0bfc5d3d58c513b222b9fd05f19724cf5127
Matthew McClaskey <matt@kasmweb.com>
2026-05-09 13:49:42 +0000

    Bring KasmVNC to tip of master

--- src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
+++ src/ubuntu/install/kasm_vnc/install_kasm_vnc.sh
@@ -10,13 +10,13 @@ prepare_rpm_repo_dependencies() {
     dnf install -y oracle-epel-release-el9
   fi
 }
-# https://kasmweb-build-artifacts.s3.amazonaws.com/kasmvnc/7180cb8989867f532234a9ca11b2249c3363cd0c/kasmvncserver_bookworm_1.4.1_feature_VNC-151-investigate-tiger-vnc-h-264-encode-decoder2-color-conversion_367266_amd64.deb
+
 echo "Install KasmVNC server"
 cd /tmp
 BUILD_ARCH=$(uname -m)
 UBUNTU_CODENAME=""
-COMMIT_ID="7180cb8989867f532234a9ca11b2249c3363cd0c"
-BRANCH="feature_VNC-151-investigate-tiger-vnc-h-264-encode-decoder2-color-conversion" # just use 'release' for a release branch
+COMMIT_ID="0a64d3381cf2ebfb53685c9c9c804e8a327bf0a3"
+BRANCH="master" # just use 'release' for a release branch
 KASMVNC_VER="1.4.1"
 COMMIT_ID_SHORT=$(echo "${COMMIT_ID}" | cut -c1-6)
 
